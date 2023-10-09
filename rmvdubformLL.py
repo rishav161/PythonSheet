d51d67130ff4c81c22fcd071fce34b859b742b40
Rishav <rishavjaiswal864@gmail.com>
2023-10-09 20:01:32 +0530

    remove duplicate element for a LinkedList

---
+++ rmvdubformLL.py
@@ -0,0 +1,11 @@
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        n=head
+        if head is None:
+            return None
+        while n is not None and n.next is not None:
+            if n.val==n.next.val:
+                n.next=n.next.next
+            else:
+                n=n.next
+        return head
