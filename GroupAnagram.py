7a3f7e3804e03cb18ac3d0c4ddd609eeb9eaaad1
Rishav <rishavjaiswal864@gmail.com>
2023-10-10 23:00:29 +0530

    group of anagram

---
+++ GroupAnagram.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d=defaultdict(list)
+        for w in strs:
+            key=''.join(sorted(w))
+            d[key].append(w)
+        return d.values()
