a5ea34b901d28288da9ff8e9e2911f5554e1c17a
Rishav <rishavjaiswal864@gmail.com>
2023-10-23 11:53:33 +0530

    Maximum sum increasing subsequence

---
+++ maxSumIncrSubSeq.py
@@ -0,0 +1,13 @@
+class Solution:
+	def maxSumIS(self, arr, n):
+		# code here
+		li=[]
+		maxsum=0
+		for i in range(n):
+		    li.append(arr[i])
+        for i in range(n):
+            for j in range(0,i):
+                if arr[i]>arr[j]:
+                    li[i]=max(li[i],li[j]+arr[i])
+            maxsum=max(li[i],maxsum)
+        return maxsum
